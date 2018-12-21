52fe095deb44485565503cab28762373f39109b9
Konsta Lehtinen <lehtinen.konsta@gmail.com>
2018-12-21 17:32:14 +0200

    Implement image piping

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const fetch = require('node-fetch');
 const app = express()
 const port = 3000
+
+const baseUrl = `https://b.tile.openstreetmap.org/`;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 
+app.get('/map/:x/:y/:z', (req, res) => {
+    
+    
+    fetch(`${baseUrl}/${req.params.z}/${req.params.x}/${req.params.y}.png`)
+        .then(response => response.buffer())
+        .then(img => {     
+            res.setHeader("Content-Type", "image/png")
+            res.end(img, 'binary'); 
+        })
+        .catch(err => {
+            console.error('error!', err);
+            res.json(err);
+        });
+
+   
+}) 
 app.listen(port, () => console.log(`Ascii world listening on port ${port}!`))
